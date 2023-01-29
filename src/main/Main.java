c3ad1209da2facf01c63e611cc111ea5f99e24c5
SebastianGuevara <sebastianguve@unisabana.edu.co>
2023-01-29 15:31:19 -0500

    Primera solución open closed

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,31 +1,27 @@
 package main;
 
+import modelo.Administracion;
 import modelo.Estudiante;
+import modelo.Industrial;
+import modelo.Informatica;
 import service.EnvioMaterial;
 
 public class Main {
     public static void main(String[] args) {
         Estudiante[] listadoEstudiantes = {
-                new Estudiante("Daniel", "Informatica"),
-                new Estudiante("Monica", "Administracion"),
-                new Estudiante("Liliana", "Industrial")
+                new Estudiante("Daniel", new Informatica()),
+                new Estudiante("Monica", new Administracion()),
+                new Estudiante("Liliana", new Industrial())
         };
         verMateriasEstudiantes(listadoEstudiantes);
         EnvioMaterial material = new EnvioMaterial();
-        material.enviarMaterialEstudiante(new Estudiante("Daniel", "Informatica"));
+        material.enviarMaterialEstudiante(new Estudiante("Daniel", new Informatica()));
     }
 
     public static void verMateriasEstudiantes(Estudiante[] estudiantes) {
-        for (Estudiante estudiante : estudiantes) {
-            if (estudiante.carrera.equals("Informatica")) {
-                System.out.println("Programacion, Arquitectura, Base de datos");
-            }
-            if (estudiante.carrera.equals("Administracion")) {
-                System.out.println("Negocios, Administracion I, Historia de la Administracion");
-            }
-            if (estudiante.carrera.equals("Industrial")) {
-                System.out.println("Procesos, Analitica de datos, Gestion de Calidad ");
-            }
+        for (Estudiante estudiante : estudiantes)
+        {
+            estudiante.carrera.consultarMaterias();
         }
     }
 }

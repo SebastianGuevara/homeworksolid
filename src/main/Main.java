f1fbf8a12376ca78716a6696638a01be2f890d20
SebastianGuevara <sebastianguve@unisabana.edu.co>
2023-01-29 19:52:25 -0500

    Segunda solucion de open closed, inyección de dependencias

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,27 +1,25 @@
 package main;
 
-import modelo.Administracion;
-import modelo.Estudiante;
-import modelo.Industrial;
-import modelo.Informatica;
+import modelo.*;
+import service.EmailOutlook;
 import service.EnvioMaterial;
 
 public class Main {
     public static void main(String[] args) {
         Estudiante[] listadoEstudiantes = {
-                new Estudiante("Daniel", new Informatica()),
-                new Estudiante("Monica", new Administracion()),
-                new Estudiante("Liliana", new Industrial())
+                new Estudiante("Daniel", new Informatica(new Envio(), new EmailOutlook())),
+                new Estudiante("Monica", new Administracion(new Envio(), new EmailOutlook())),
+                new Estudiante("Liliana", new Industrial(new Envio(), new EmailOutlook()))
         };
         verMateriasEstudiantes(listadoEstudiantes);
         EnvioMaterial material = new EnvioMaterial();
-        material.enviarMaterialEstudiante(new Estudiante("Daniel", new Informatica()));
+        material.enviarMaterialEstudiante(new Estudiante("Daniel", new Informatica(new Envio(), new EmailOutlook())));
     }
 
     public static void verMateriasEstudiantes(Estudiante[] estudiantes) {
         for (Estudiante estudiante : estudiantes)
         {
-            estudiante.carrera.consultarMaterias();
+            estudiante.carrera.consultarMateriasCarrera();
         }
     }
 }

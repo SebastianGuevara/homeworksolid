3175e0ced4026df1fa95fb43cb68f628be3e2352
SebastianGuevara <sebastianguve@unisabana.edu.co>
2023-01-30 10:19:20 -0500

    Se agregaron interfaces para cumplir el principio de responsabilidad única en la clase estudiante y para inversión de dependencia

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,20 +3,22 @@ package main;
 import modelo.*;
 import service.EmailOutlook;
 import service.EnvioMaterial;
+import service.ServidoresEmail;
 
 public class Main {
     public static void main(String[] args) {
         Estudiante[] listadoEstudiantes = {
-                new Estudiante("Daniel", new Informatica(new Envio(), new EmailOutlook())),
-                new Estudiante("Monica", new Administracion(new Envio(), new EmailOutlook())),
-                new Estudiante("Liliana", new Industrial(new Envio(), new EmailOutlook()))
+                new Estudiante("Daniel", new Informatica(new Envio(), new EmailOutlook(new ServidoresEmail()))),
+                new Estudiante("Monica", new Administracion(new Envio(), new EmailOutlook(new ServidoresEmail()))),
+                new Estudiante("Liliana", new Industrial(new Envio(), new EmailOutlook(new ServidoresEmail())))
         };
         verMateriasEstudiantes(listadoEstudiantes);
         EnvioMaterial material = new EnvioMaterial();
-        material.enviarMaterialEstudiante(new Estudiante("Daniel", new Informatica(new Envio(), new EmailOutlook())));
+        material.enviarMaterialEstudiante(new Estudiante("Daniel", new Informatica(new Envio(), new EmailOutlook(new ServidoresEmail()))));
     }
 
-    public static void verMateriasEstudiantes(Estudiante[] estudiantes) {
+    public static void verMateriasEstudiantes(Estudiante[] estudiantes)
+    {
         for (Estudiante estudiante : estudiantes)
         {
             estudiante.carrera.consultarMateriasCarrera();
